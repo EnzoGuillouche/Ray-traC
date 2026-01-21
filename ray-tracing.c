5a272d8e626e35001b0b99cfec637d8d95e02376
EnzoGuillouche <145991192+EnzoGuillouche@users.noreply.github.com>
2026-01-21 01:05:30 +0100

    entities are setup, now comes the big part

--- ray-tracing.c
+++ ray-tracing.c
@@ -1,14 +1,48 @@
 #include "raylib.h"
 
+#define WIDTH 800
+#define HEIGHT 450
+
+#define NUM_ENTITIES 2
+
+typedef struct Entity {
+    int x, y;
+    int radius;
+    Color color;
+    bool isEmmiter; // is a light source
+} Entity;
+
+Entity entities[NUM_ENTITIES];
+
+void InitEntities()
+{
+    entities[0] = (Entity) { 
+        200, 200, 50, RED, true
+    };
+
+    entities[1] = (Entity) { 
+        600, 200, 75, BLUE, false
+    };
+}
+
+void DrawEntities()
+{
+    for (int i = 0; i < NUM_ENTITIES; i++)
+    {
+        DrawCircle(entities[i].x, entities[i].y, entities[i].radius, entities[i].color);
+    }
+}
+
 int main(void)
 {
-    InitWindow(800, 450, "raylib [core] example - basic window");
+    InitWindow(WIDTH, HEIGHT, "Ray Tracing in C");
+    InitEntities();
 
     while (!WindowShouldClose())
     {
         BeginDrawing();
             ClearBackground(BLACK);
-            DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
+            DrawEntities();
         EndDrawing();
     }
 
